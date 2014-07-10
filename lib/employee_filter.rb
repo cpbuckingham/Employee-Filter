ef566b867c4f18591c7fc90682985d118bbb32e9
Cameron <cameron.p.buckingham@gmail.com>
2014-07-10 09:55:56 -0600

    done

--- lib/employee_filter.rb
+++ lib/employee_filter.rb
@@ -1,3 +1,29 @@
 class EmployeeFilter
 
+  def initialize(employees)
+    @employees = employees
+  end
+
+
+#need an array here
+  def started_before_2006
+    before_2006 = []
+    @employees.each do |employee_hash|
+      before_2006 << employee_hash unless employee_hash[:start_date].year >= 2006
+    end
+    before_2006
+  end
+
+#need a hash here
+  def all_with_start_date
+    info = []
+    @employees.each do |employee_hash|
+      first = employee_hash[:first_name]
+      last = employee_hash[:last_name]
+      title = employee_hash[:title]
+      start_date = employee_hash[:start_date]
+      info << "#{first} #{last} (#{title}) - #{start_date.month}/#{start_date.day}/#{start_date.year}"
+    end
+    info
+  end
 end
